4020ae5138c737d08bf9cb6bcbf28431e95d79fe
Grant <yinguolin@aliyun.com>
2019-09-04 22:10:14 +0800

    simple material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
+import 'model/post.dart';
 
 void main() => runApp(App());
 
-// 自定义部件
+
 
 class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.title,
+          ),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          SizedBox(height: 12.0)
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('GRANT'),
+          elevation: 0,
+        ),
+        body: ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: _listItemBuilder,
+        ),
+      );
+  }
+}
+
+
+class Hello extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -13,9 +66,10 @@ class App extends StatelessWidget {
         textDirection: TextDirection.ltr,
         style: TextStyle(
           fontSize: 40.0,
-          fontWeight: FontWeight.w100
+          fontWeight: FontWeight.w100,
+          color: Colors.pink
         ),
       ),
-    );
+    );;
   }
 }
